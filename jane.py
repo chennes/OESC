eefd8c40f13353d3dde2bf8602ec79056bd7e424
Jane Stout <Jane.Stout@oesc.ok.gov>
2024-10-24 10:12:42 -0500

    Initial version of jane.py

---
+++ jane.py
@@ -0,0 +1,16 @@
+products = [
+    "pepperoni",
+    "sausage",
+    "cheese",
+    "green peppers",
+    "white sauce",
+    "red sauce",
+    "thin crust"
+]
+
+def print_menu():
+    for food in products:
+        print(food)
+
+if __name__ == "__main__":
+    print_menu()
