5c361fcc13cdd5cd1f4a9a8698ed29e775ca98fa
Nahid Chafi <nahid.chafi@oesc.gov>
2024-10-24 10:33:15 -0500

    Add create pizza function

--- nahid.py
+++ nahid.py
@@ -11,5 +11,21 @@ products = [
 def print_menu() -> None:
     for food in products:
         print(food)
+
+
+
+def create_pizza() -> list[str]:
+    print("Please create your pizza now:")
+    ingredients = []
+    while True:
+        ingredient = input("Ingredient: ")
+        if ingredient.lower() in products:
+            ingredients.append(ingredient.lower())
+        elif ingredient.lower() == "done":
+            return ingredients
+        else:
+            print(f"Sorry, we dont have {ingredient}")    
+
 if __name__ == "__main__":
-    print_menu()        
+    print_menu() 
+    create_pizza()       
