e40594d78536245db046c0c8ab23334714c10f8a
Nahid Chafi <nahid.chafi@oesc.gov>
2024-10-24 10:12:46 -0500

    Initial version of nahid.py

---
+++ nahid.py
@@ -0,0 +1,15 @@
+products = [
+    "pepproni",
+    "sausage",
+    "green peppers",
+    "red sauce",
+    "white sauce",
+    "thin crust",
+    "thick crust" 
+
+]
+def print_menu() -> None:
+    for food in products:
+        print(food)
+if __name__ == "__main__":
+    print_menu()        
