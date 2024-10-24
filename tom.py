5f16006a61ceb8f677ddc0508975c3d252514ba2
Tom O <tom.o'laughlin@oesc.ok.gov>
2024-10-24 10:32:50 -0500

    Add create pizza function

--- tom.py
+++ tom.py
@@ -13,5 +13,20 @@ def print_menu() -> None:
     for food in products:
         print(food)
 
+def create_pizza() -> list[str]:
+    """docstring goes here"""
+    print("Please create your pizza now:")
+    ingredients = []
+    while True:
+        ingredient = input("Ingredient: ")
+        if ingredient.lower() in products:
+            ingredients.append(ingredient.lower())
+        elif ingredient.lower() == "done":
+            return ingredients
+        else:
+            print(f"Sorry, we don't have {ingredient}.")
+
+
 if __name__ == "__main__":
     print_menu()
+    create_pizza()
