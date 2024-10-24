73c2ecd155b43b138fccc486d7834b7b6d35b5e4
Gary Long <127325012+Gary-OESC@users.noreply.github.com>
2024-10-24 10:58:49 -0500

    change 1

--- GARY.py
+++ GARY.py
@@ -12,6 +12,20 @@ def print_menu() -> None:
     for food in products:
         print(food)
 
+def create_pizza() -> list[str]:
+    print ("Please create your pizza now:")
+    ingredients = []
+    while True:
+        ingredient = input("ingredient: ")
+        if ingredient.lower() in products:
+            ingredients.append(ingredient.lower)
+        elif ingredient.lower() == "done":
+            return ingredients
+        else:
+            print(f"Sorry, we don't have that {ingredient}")
+
+
 if __name__ == "__main__":
     print_menu()
+    create_pizza()
 
