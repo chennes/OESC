b4adeea0ef0dab764c2f25641f6d274a9e74423d
Chris Hennes <chennes@pioneerlibrarysystem.org>
2024-10-24 10:32:31 -0500

    Add create_pizza function

--- chris.py
+++ chris.py
@@ -12,5 +12,19 @@ def print_menu() -> None:
     for food in products:
         print(food)
 
+def create_pizza() -> list[str]:
+    print("Please create your pizza now:")
+    ingredients = []
+    while True:
+        ingredient = input("Ingredient: ")
+        if ingredient.lower() in products:
+            ingredients.append(ingredient.lower())
+        elif ingredient.lower() == "done":
+            return ingredients
+        else:
+            print(f"Sorry, we don't have {ingredient}. Fool.")
+
+
 if __name__ == "__main__":
     print_menu()
+    create_pizza()
