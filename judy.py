5f0b82f9d04d70fcc92c9ca179a01b727534d520
Judy Lewis <judy.lewis@oesc.ok.gov>
2024-10-24 10:32:36 -0500

    Add create pizza functino

--- judy.py
+++ judy.py
@@ -12,5 +12,18 @@ def print_menu() -> None:
     for food in products:
         print(food)
 
+def create_pizza() -> list[str]:
+    print("Please create your pizza now:")
+    ingredients = []
+    while True:                                      #infinite loop format
+        ingredient = input("Ingredient: ")
+        if ingredient.lower() in products:
+            ingredients.append(ingredient.lower())
+        elif ingredient.lower() == "done":
+            return ingredients
+        else:
+            print(f"Sorry, we dont' have {ingredient}")
+
 if __name__ == "__main__":
     print_menu()
+    create_pizza()
