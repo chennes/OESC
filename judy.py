b77b3c5d203cc496e0df4e6a49b1d20ca4b51ef1
Judy Lewis <judy.lewis@oesc.ok.gov>
2024-10-24 10:09:28 -0500

    Initial version of judy.py

---
+++ judy.py
@@ -0,0 +1,16 @@
+products = [             #global variable
+    "pepperoni",
+    "sausage",
+    "green peppers",
+    "red sauce",
+    "white sauce",
+    "thin crust",
+    "thick crust"
+]
+
+def print_menu() -> None:
+    for food in products:
+        print(food)
+
+if __name__ == "__main__":
+    print_menu()
