ebc03ddfa6d393328135688ec85f55db146b5e9d
Nahid Chafi <nahid.chafi@oesc.gov>
2024-11-04 10:39:16 -0600

    Add create_pizza function

--- nahid.py
+++ nahid.py
@@ -12,11 +12,23 @@ products = [
 def print_menu() -> None:
     for food in products:
         print(food)
-
-
+#returns a list of strings
+def create_pizza() -> list[str]:
+    print("Please create your pizza now:")
+# need a loop that would requir a done to end  while loop . ingredients is a storage location we need    
+    ingredients = []
+    while True:
+        ingredient = input("Ingredient: ")
+        if ingredient.lower() in products:
+            ingredients.append(ingredient.lower())
+        elif ingredient.lower() == "done":
+            return ingredients
+        else:
+            print(f"Sorry, we dont have {ingredient}")  
 
   
 
 if __name__ == "__main__":
     print_menu() 
+    create_pizza()
           
